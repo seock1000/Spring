29e7bec07cd38a73eafdaae313bc578d8a70ed9c
seock1000 <tjrcjs0705@gmail.com>
2023-01-01 21:01:09 +0900

    basic Object Orient Design Practice

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // 동시성 이슈 때문에 ConcurrentHashMap 써야 하지만 예제이므로 HashMap 사용
+    private static Map<Long, Member> store = new HashMap<Long, Member>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
